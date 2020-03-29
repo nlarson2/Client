67c8ff69ac0a505f98a9e3b8c0c72413cd79f848
Brandon Hernandez <brandonlive98@hotmail.com>
2020-03-28 23:07:36 -0700

    Imported Game Scene
    Start of merge

---
+++ Assets/Game Scenes/Prefabs/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleShoot : MonoBehaviour
+{
+
+    public SteamVR_Action_Boolean fireAction;
+    public GameObject bulletPrefab;
+    public GameObject casingPrefab;
+    public GameObject muzzleFlashPrefab;
+    public Transform barrelLocation;
+    public Transform casingExitLocation;
+
+    public float shotPower = 100f;
+
+    private Interactable interactable;
+    private Animator animator;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        animator = GetComponent<Animator>();
+        if (barrelLocation == null)
+            barrelLocation = transform;
+    }
+
+    void Update()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+            if(fireAction[source].stateDown) {
+                //Shoot();
+                //CasingRelease();
+                animator.Play("Shooting",0,0);
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        //  GameObject bullet;
+        //  bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        // bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+
+        GameObject tempFlash;
+       Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+       tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+
+       // Destroy(tempFlash, 0.5f);
+        //  Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
+       
+    }
+
+    void CasingRelease()
+    {
+         GameObject casing;
+        casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
+        casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
+        casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
+    }
+
+
+}
---
+++ Assets/Game Scenes/Scripts/Control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Control : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void NextScene()
+    {
+        Destroy(Player);
+        SceneManager.LoadScene("Test Room");    
+    }
+}
